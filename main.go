dcc7260bd11c05fa3f0a508bb6ed5408d0b850dd
ZNing <zhn038@gmail.com>
2022-01-14 23:42:35 +0800

    Update main.go

--- main.go
+++ main.go
@@ -70,5 +70,49 @@ func main() {
 		c.SaveUploadedFile(headers, "./video/"+headers.Filename)
 		c.String(http.StatusOK, headers.Filename)
 	})
+
+	// 限制表单上传大小 8MB，默认为32MB
+	r.MaxMultipartMemory = 8 << 20
+	r.POST("/upload3", func(c *gin.Context) {
+		form, err := c.MultipartForm()
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("get err %s", err.Error()))
+		}
+		// 获取所有图片
+		files := form.File["files"]
+		// 遍历所有图片
+		for _, file := range files {
+			// 逐个存
+			if err := c.SaveUploadedFile(file, file.Filename); err != nil {
+				c.String(http.StatusBadRequest, fmt.Sprintf("upload err %s", err.Error()))
+				return
+			}
+		}
+		c.String(200, fmt.Sprintf("upload ok %d files", len(files)))
+	})
+
+	// 路由组1 ，处理GET请求
+	v1 := r.Group("/v1")
+	// {} 是书写规范
+	{
+		v1.GET("/login", login)
+		v1.GET("submit", submit)
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.POST("/login", login)
+		v2.POST("/submit", submit)
+	}
+
 	r.Run(":8000")
+}
+
+func login(c *gin.Context) {
+	name := c.DefaultQuery("name", "jack")
+	c.String(200, fmt.Sprintf("hello %s\n", name))
+}
+
+func submit(c *gin.Context) {
+	name := c.DefaultQuery("name", "lily")
+	c.String(200, fmt.Sprintf("hello %s\n", name))
 }
